f76fe4311f91d465895ee612155061dade670a55
YUBBY26 <heyubar26@gmail.com>
2025-03-02 14:25:46 +0200

    Meme Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-function App() {
+import Header from "../Components/Header"
+import Meme from "../Components/Meme"
+export default function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+        <Header />
+        <Meme />
+    </div>
   )
 }
-
-export default App
